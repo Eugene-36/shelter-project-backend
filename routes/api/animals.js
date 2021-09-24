aa9e5428930ce5b44e5addbe6019323409fc9a6d
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-09-24 12:26:24 +0300

    delete log

--- routes/api/animals.js
+++ routes/api/animals.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Animals = require("../../model/animals");
-// const {
-//   validationCreateCat,
-//   validationUpdateCat,
-//   validationUpdateStatusCat,
-// } = require("./validation");
 
 router.use((req, res, next) => {
   console.log(req.url);
@@ -13,9 +8,10 @@ router.use((req, res, next) => {
 });
 
 router.get("/", async (req, res, next) => {
-  console.log("Hi");
+  //console.log("Hi");
   try {
     const animals = await Animals.getAll();
+    log(animals);
     return res.json({ status: "success", code: 200, data: { animals } });
   } catch (e) {
     next(e);
