f53c5b714ab201430fbd2b38720534d90216b4f2
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-09-24 17:19:53 +0300

    deleted console

--- routes/api/animals.js
+++ routes/api/animals.js
@@ -2,16 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Animals = require("../../model/animals");
 
+
 router.use((req, res, next) => {
   console.log(req.url);
   next();
 });
 
 router.get("/", async (req, res, next) => {
-  //console.log("Hi");
   try {
     const animals = await Animals.getAll();
-    log(animals);
     return res.json({ status: "success", code: 200, data: { animals } });
   } catch (e) {
     next(e);
